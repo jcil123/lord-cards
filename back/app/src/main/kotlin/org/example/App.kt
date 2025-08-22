df39a8b0aa46ad73ddbfb0fc928ace3cfd887b85
JC <jciler2003@gmail.com>
2025-08-23 00:29:45 +0200

    Perron

--- back/app/src/main/kotlin/org/example/App.kt
+++ back/app/src/main/kotlin/org/example/App.kt
@@ -6,7 +6,7 @@ package org.example
 class App {
     val greeting: String
         get() {
-            return "Hello World Puto!"
+            return "Hello World, First Correct Commit!"
         }
 }
 
